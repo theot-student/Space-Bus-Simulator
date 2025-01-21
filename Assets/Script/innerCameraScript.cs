64648958bd1fdb6ede3c8b04699f84786ffbf666
Theotim Barbier <tbarbierlaffitt@mahr203-09.ad.insa-rouen.fr>
2025-01-21 11:47:10 +0100

    test

---
+++ Assets/Script/innerCameraScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class innerCameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
