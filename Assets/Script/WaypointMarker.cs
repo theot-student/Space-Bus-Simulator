b7a252956f2e341ea571e43f657c64cd545ad357
theotim <theotim.bartbier@gmail.com>
2025-02-05 21:20:43 +0100

    waypoint fixed

--- Assets/Script/WaypointMarker.cs
+++ Assets/Script/WaypointMarker.cs
@@ -7,29 +7,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Transform target;
     public TextMeshProUGUI meterText;
     public Vector3 offset;
+    public Camera camera;
     void Update()
     {   
-        float minX = img.GetPixelAdjustedRect().width / 2;
-        float maxX = Screen.width - minX;
+        if (camera.enabled) {
+            float minX = img.GetPixelAdjustedRect().width / 2;
+            float maxX = Screen.width - minX;
 
-        float minY = img.GetPixelAdjustedRect().height / 2;
-        float maxY = Screen.height - minX;
+            float minY = img.GetPixelAdjustedRect().height / 2;
+            float maxY = Screen.height - minX;
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position) + offset;
-        
-        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0){
-            if (pos.x < Screen.width / 2){
-                pos.x = maxX;
-            } else {
-                pos.x = minX;
+            Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
+            
+            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0){
+                if (pos.x < Screen.width / 2){
+                    pos.x = maxX;
+                } else {
+                    pos.x = minX;
+                }
             }
-        }
- 
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+    
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-        img.transform.position = pos;
-        meterText.text = ((int) Vector3.Distance(target.position, transform.position) + "m");
- 
+            img.transform.position = pos;
+            meterText.text = ((int) Vector3.Distance(target.position, transform.position) + "m");
+    }
     }
 }
