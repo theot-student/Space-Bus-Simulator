a328b39adaacb2ec33ed923e157e056243869944
theotim <theotim.bartbier@gmail.com>
2025-02-11 19:51:47 +0100

    pause menu

--- Assets/Script/PauseGameScript.cs
+++ Assets/Script/PauseGameScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PauseGameScript : MonoBehaviour
 {
     public static bool gameIsPaused = false;
-
+    public GameObject pauseMenuUI;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -17,12 +17,29 @@ public class PauseGameScript : MonoBehaviour
     {
         if(gameIsPaused)
         {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            pauseMenuUI.SetActive(true);
             Time.timeScale = 0f;
         }
         else 
         {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            pauseMenuUI.SetActive(false);
             Time.timeScale = 1;
         }
+    }
+    public void ResumeGame () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame (){
+        Debug.Log("GoodBye");
+        Application.Quit();
     }
 }
 
